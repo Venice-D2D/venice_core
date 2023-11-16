b6e0bfac2b1ccdf4a53d0d1080a8ee783da699da
Remy Raes <contact@remyraes.com>
2023-11-16 08:13:27 +0100

    fix: use Uint8List type

--- lib/network/message.dart
+++ lib/network/message.dart
@@ -1,24 +1,25 @@
 import 'dart:ffi';
+import 'dart:typed_data';
 
 /// This class represents metadata coming with each chunk sent on the network
 /// through a channel.
 class VeniceMessage {
-  final Uint32 messageId;
+  final int messageId;
 
   final bool ack;
 
   // 2^18 = 256kB according to BitTorrent BEP3 : http://www.bittorrent.org/beps/bep_0003.html
-  final Uint32 size;
+  final int size;
 
   final List<Uint8> data;
 
   VeniceMessage(this.messageId, this.ack, this.size, this.data);
 
-  factory VeniceMessage.fromBytes(List<Uint8> bytes) {
+  factory VeniceMessage.fromBytes(Uint8List bytes) {
     throw UnimplementedError();
   }
 
-  List<Uint8> toBytes() {
+  Uint8List toBytes() {
     throw UnimplementedError();
   }
 }
